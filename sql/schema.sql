e662ff4808a167a4893ebd35bcad236fe51e3fef
robparkess <114871696+robparkess@users.noreply.github.com>
2026-01-15 15:36:41 +0000

    Add schema file placeholder

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+-- schema.sql
+-- Creates the nba schema and base tables used in this project.
+
+CREATE SCHEMA IF NOT EXISTS nba;
+
+-- TODO: Add CREATE TABLE statements for:
+-- nba.games
+-- nba.players
+-- nba.player_statistics
+-- nba.teams
